58bb601cd3e2651d49f84358456c0f2dd994e971
404akhan <404akhan@gmail.com>
2022-10-20 20:21:27 +0600

    modify plot

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-from dataclasses import dataclass
-from sortedcontainers import SortedList
-import matplotlib.pyplot as plt
+class Solution:
+    def countSubarrays(self, nums, minK, maxK):
+        def ct(minK, maxK):
+            ans, c = 0, 0
+            for k in nums:
+                if k > maxK or k < minK:
+                    ans += c * (c - 1) // 2 + c
+                    c = 0
+                else:
+                    c += 1
+            ans += c * (c - 1) // 2 + c
+            return ans
+
+        if minK > maxK:
+            return 0
+
+        ans = ct(minK, maxK)
+        ans -= ct(minK + 1, maxK)
+
+        if minK != maxK:
+            ans -= ct(minK, maxK - 1)
+            ans += ct(minK + 1, maxK - 1)
+
+        return ans
 
 
-arr = [1, 2, 3, 4]
-plt.title('he')
-plt.plot(arr, arr, '.g')
-plt.show()
-it = iter(arr)
-
-for i in range(5):
-    val = next(it)
-    print(val)
-
-sl = SortedList([(1, 0), (2, 3)])
-
-print(sl.pop(0))
-print(sl.pop(0))
-print(sl.pop(0))
-
-d = {'a': 1, 'c': 3}
-
-
-for v in d.values():
-    print(v)
+s = Solution()
+print(s.countSubarrays(nums = [1,3,5,2,7,5], minK = 1, maxK = 5))
+print(s.countSubarrays(nums = [1,1,1,1], minK = 1, maxK = 1))
